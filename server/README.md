05ef9c4b8e8aea22b3691e2708c7be5485476f88
Jack Jazrawy-Brown <jackjazb@gmail.com>
2025-05-21 22:25:47 +0100

    switch to remote management

--- server/README.md
+++ server/README.md
@@ -2,9 +2,25 @@
 
 Config for `shire`.
 
-## Tasks
+## Setup
 
-Post setup, `task` should be installed. Use `task --list-all` for a summary of available management tasks.
+Before doing anything, set up SSH public key auth and [Tailscale](https://tailscale.com/kb/1031/install-linux).
+
+Any server system dependencies are configured in `setup.sh`, which can be run as follows:
+
+```bash
+export remote=root@shire.bear-antares.ts.net # Amend as necessary.
+rsync ./setup.sh $remote$:~/setup.sh
+ssh $remote "source setup.sh"
+```
+
+## Management
+
+The Taskfile in this directory allows remote management of the server. Public key access to the root user is required for most tasks.
+
+Install `task` with `npm install -g @go-task/cli`.
+
+Use `task --list-all` for a summary of available management tasks.
 
 ## Services
 
@@ -15,3 +31,5 @@ handle_oath /servicepath/* {
     reverse_proxy :1234
 }
 ```
+
+Then run `task caddy` to load the new config.
--- server/setup.sh
+++ server/setup.sh
@@ -1,13 +1,24 @@
-sudo cp ./authorized_keys ~/.ssh/authorized_keys
+#
+# Setup script - to be run on the target server.
+#
 
+# Docker setup (Debian)
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Additional setup
 sudo apt -y install vim curl caddy
-sudo cp ./Caddyfile /etc/caddy/Caddyfile
-
-curl https://mise.run | sh
-echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
-. ~/.bashrc
-npm install -g @go-task/cli
-
-# Jellyfin
-mkdir -p ~/jellyfin/media/music
-
+mkdir -p {{.dir}}/jellyfin/media/music

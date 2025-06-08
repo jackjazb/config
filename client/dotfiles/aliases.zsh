5c81ead9286450eff95830565a13eca4a451e437
Jack Jazrawy-Brown <jackjazb@gmail.com>
2025-06-08 16:51:44 +0100

    alias cd

--- client/dotfiles/aliases.zsh
+++ client/dotfiles/aliases.zsh
@@ -1,6 +1,7 @@
 alias python="python3"
 alias docker="sudo docker"
 alias gc="git commit -m"
+alias cd="z"
 
 function dps() {
     docker ps $1 --format '{"ID":"{{ .ID }}", "Image": "{{ .Image }}", "Names":"{{ .Names }}", "Ports":"{{ .Ports }}"}' | jq
